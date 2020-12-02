e024f100fae00ebdae39e145789fbdd6bfb4aa1d
Rajas Kulkarni <rajas.aveo@gmail.com>
2020-12-02 20:46:09 +0530

    [FIX] pakages

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def home_view(): 
-		return "<h1>Welcome to Geeks for Geeks</h1>"
+		return "<h1>Yeah, the site works</h1>"
 
 @app.route('/bot', methods=['POST'])
 def bot():
